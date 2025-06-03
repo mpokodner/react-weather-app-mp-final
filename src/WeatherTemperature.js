ad6765efc162a7b18db4e011ac7442c7da4e7af0
mpo.coder <mpokodner@gmail.com>
2025-06-03 16:48:54 -0500

    convert metric to imperial

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+  function convertToCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          <a href="/" onClick={convertToFahrenheit}>
+            {" "}
+            °C
+          </a>{" "}
+          |{" "}
+          <a href="/" onClick={convertToCelsius}>
+            °F
+          </a>{" "}
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="unit">
+          <a href="/" onClick={convertToCelsius}>
+            {" "}
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
 }
