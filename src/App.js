57b78dfb3555108ed68e9fcba02edd9da98d8a9b
mpo.coder <mpokodner@gmail.com>
2025-06-03 13:28:42 -0500

    updated app.js to include weather data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import WeatherData from "./WeatherData";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather City Search</h1>
+      <div className="App">
+        <WeatherData />
+      </div>
     </div>
   );
 }
