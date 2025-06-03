dcb82003dbe6ea5f55638be30584e27ad864b5ef
mpo.coder <mpokodner@gmail.com>
2025-06-03 17:33:58 -0500

    coord or coordinates

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -10,7 +10,7 @@ export default function WeatherData(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
-      coordinates: response.data.coordinates,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
