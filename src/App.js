5efc2fbf3c4fc7488a9fea7161e5844ce6f89aca
mpo.coder <mpokodner@gmail.com>
2025-06-04 08:16:43 -0500

    imported weatherdata into app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ReactAnimatedWeather from "react-animated-weather";
+import WeatherData from "./WeatherData";
 
 // App.js
 function App() {
